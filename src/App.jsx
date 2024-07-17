ba3b6cb791c30e65a2cbb488888a4a2dd9930bd0
Irina Litvinova <neforsha@yandex.ru>
2024-07-17 13:50:14 +0700

    feat: added How It Works and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { Hero } from "./components/Hero";
 import { Highlights } from "./components/Highlights";
 import { Navbar } from "./components/Navbar";
 import { Model } from "./components/Model";
-import * as Sentry from '@sentry/react'
+import * as Sentry from "@sentry/react";
 import { Features } from "./components/Features";
-
+import { HowItWorks } from "./components/HowItWorks";
+import { Footer } from "./components/Footer";
 
 const App = () => {
   return (
@@ -14,6 +15,8 @@ const App = () => {
       <Highlights />
       <Model />
       <Features />
+      <HowItWorks />
+      <Footer />
     </main>
   );
 };
